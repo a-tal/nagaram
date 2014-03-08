4d8581a4f438203d94a23061fdfb7e7b8093e893
a-tal <github@talsma.ca>
2014-03-07 23:59:12 -0800

    give unittest2 to py2.6

--- test/unit/scrabble_tests.py
+++ test/unit/scrabble_tests.py
@@ -5,7 +5,12 @@
 
 
 import os
-import unittest
+import sys
+
+if sys.version_info <= (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
 
 from nagaram.scrabble import (
     blank_tiles,
